75e8553a18204665b4882277b8ca5c9c38898059
george <kesaev@gmail.com>
2020-09-22 13:14:57 +0300

    change request schema to https

--- webhoseio/__init__.py
+++ webhoseio/__init__.py
@@ -13,8 +13,8 @@ class Session(object):
         if param_dict is not None:
             param_dict.update({"token": self.token})
             param_dict.update({"format": "json"})
-       
-        response = self.session.get("http://webhose.io/" + end_point_str, params=param_dict)
+
+        response = self.session.get("https://webhose.io/" + end_point_str, params=param_dict)
         if response.status_code != 200:
             raise Exception(response.text)
 
